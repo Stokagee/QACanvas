32a183597a56b00bcfa543f16e681f00a09af539
Dušan Čižmarik <stokagecizmarik@gmail.com>
2026-05-11 18:29:16 +0200

    change QA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/qacanvas/',
+  base: '/QAcanvas/',
   build: {
     rollupOptions: {
       input: {
